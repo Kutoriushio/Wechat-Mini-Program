1d653ab96dbce5fcf2df87b956d8ff904a8952ea
Kutoriushio <328752344@qq.com>
2023-07-03 18:37:19 -0400

    跳转详情页
    跳转详情页

---
+++ pages/detail/detail.js
@@ -0,0 +1,74 @@
+// pages/detail/detail.js
+const app = getApp();
+const recipes = app.globalData.recipes;
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+        let selectedId = options.id;
+        const selectedRecipe = recipes.find(function(recipe){
+            return recipe.id == selectedId;
+        });
+        this.setData({
+            recipe: selectedRecipe
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
