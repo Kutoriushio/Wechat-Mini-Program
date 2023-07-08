1f45f98e56ad19a49737695db29878999b6da65f
Kutoriushio <328752344@qq.com>
2023-07-08 16:04:55 -0400

    本地数据存储
    打算改用服务器存储，留档

--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -1,12 +1,18 @@
 // pages/menu/menu.js
-const app = getApp();
-const categories = app.globalData.categories;
-const recipes = app.globalData.recipes;
+
 
 Page({
+    data: {
+        selectedRecipes: [],
+        recipes: [],
+        categories: [],
+        curIndex: []
+    },
+
     getRecipes: function(category) {
+        // 获取当前种类的所有食谱
         let selectedRecipe = [];
-        recipes.forEach(function(item, index){
+        this.data.recipes.forEach(function(item, index){
             if (item.category === category) {
                 selectedRecipe.push(item);
             }
@@ -15,31 +21,79 @@ Page({
     },
 
     getCurrentCategory: function(e) {
+        // 响应用户的点击事件，显示对应种类的所有食谱
         let index = e.currentTarget.dataset.index;
-        let category = categories[index].name;
+        let category = this.data.categories[index].name;
 
-        let selectedRecipe = [];
-        selectedRecipe = this.getRecipes(category);
+        let selectedRecipes = [];
+        selectedRecipes = this.getRecipes(category);
+        // 把对应种类的食谱数据传给全局变量selectedRecipes
+        this.data.selectedRecipes = selectedRecipes;
         this.setData({
             curIndex: index,
-            selectedRecipe: selectedRecipe
+            selectedRecipes: selectedRecipes
         })
     },
     
     onLoad: function() {
-        const initialRecipe = this.getRecipes(categories[0].name)
-        this.setData({
-            categories: categories,
+        // 初始化函数
+        const app = getApp();
+        // 给全局数据的recipes添加key(clickOrder)
+        app.globalData.recipes.map((item) => {
+            return {
+                ...item,
+                clickOrder: 0
+            };
+        });
+        this.data.categories = app.globalData.categories;
+        this.data.recipes = app.globalData.recipes;
+        // 显示第一个种类的菜谱
+        const initialRecipe = this.getRecipes(this.data.categories[0].name);
+        this.setData({  
+            categories: this.data.categories,
             curIndex: 0,
-            selectedRecipe: initialRecipe
+            selectedRecipes: initialRecipe
+        })
+    },
+
+    onShow: function() {
+        // 更新全局数据
+        this.setData({
+            recipes: getApp().globalData.recipes
+        });
+        // 重新渲染页面
+        const index = this.data.curIndex;
+        const renderedRecipes = this.getRecipes(this.data.categories[index].name);
+        this.setData({
+            selectedRecipes: renderedRecipes
         })
     },
 
     goToDetailPage: function(e) {
+        // 跳转至详情页
         let curId = e.currentTarget.dataset.id;
-        console.log(curId);
         wx.navigateTo({
           url: '/pages/detail/detail?id=' + curId,
+        })
+    },
+
+    addToOrder: function(e) {
+        // 响应用户的点击事件，将选中的食谱添加到点菜中
+        let curId = e.currentTarget.dataset.id;
+        // 修改点击的菜谱的标签内容并以时间排序
+        const selectedRecipes = this.data.selectedRecipes.map((recipe) =>{
+            if(recipe.id === curId) {
+                if (recipe.buttonText === '添加') {
+                    recipe.buttonText = '已添加';
+                    recipe.clickOrder = Date.now();
+                } else {
+                    recipe.buttonText = '添加'
+                }
+            }
+            return recipe
+        });
+        this.setData({
+            selectedRecipes
         })
     }
 })
--- pages/order/order.js
+++ pages/order/order.js
@@ -5,14 +5,22 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        recipes: [],
+        addedRecipes: [],
+        startX: 0,
+        startY: 0,
+        scrollY : true
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-
+        const app = getApp();
+        const recipes = app.globalData.recipes;
+        this.setData({
+            recipes
+        })
     },
 
     /**
@@ -26,7 +34,10 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow() {
-
+        const updatedRecipes = this.updateOrder();
+        this.setData({
+            addedRecipes: updatedRecipes
+        });
     },
 
     /**
@@ -62,5 +73,77 @@ Page({
      */
     onShareAppMessage() {
 
+    },
+
+    updateOrder: function() {
+        // 选中所有已添加的食谱，并按照点击顺序排序
+        const updatedRecipes = this.data.recipes.filter(recipe => recipe.buttonText === '已添加');
+        const sortedRecipes = updatedRecipes.sort((a,b) => {
+            return a.clickOrder - b.clickOrder;
+        });
+        return sortedRecipes;
+    },
+
+    touchStart: function(e) {
+        //给数组添加一对键值对，表示是否显示删除选项
+        this.data.addedRecipes = this.data.addedRecipes.map((item) => {
+            return {
+                ...item,
+                show: false
+            }
+        });
+        this.setData({
+            startX : e.touches[0].clientX,
+            startY : e.touches[0].clientY
+        });
+    },
+
+    touchMove: function(e) {
+        const curIndex = e.currentTarget.dataset.index;
+        let moveX = e.changedTouches[0].clientX;
+        let moveY = e.changedTouches[0].clientY;
+        let addedRecipes = this.data.addedRecipes;
+        let scrollY = true;
+        // 计算X和Y的位移
+        const deltaX = moveX - this.data.startX;
+        const deltaY = moveY - this.data.startY;
+        // 计算角度
+        const angle = 360 * Math.atan(deltaY / deltaX) / (2 * Math.PI);
+        addedRecipes.forEach((item, index) => {
+            // 角度大于30表示用户上滑，直接返回
+            if (angle > 30) {
+                return;
+            }
+            //遍历数组，如果用户触碰目标的index和页面数组的index一致且是向左滑动，就显示删除选项
+            //touches.clientX的原点在左上角
+            if (curIndex === index) {
+                if (moveX + 100 < this.data.startX) {
+                    item.show = true;
+                    scrollY = false;
+                }
+            }
+        })
+        this.setData({
+            addedRecipes,
+            scrollY: scrollY
+        })
+    },
+
+    delete: function(e) {
+        const btnIndex = e.currentTarget.dataset.index;
+        // 找到要删除的菜谱
+        let deleteRecipe = this.data.addedRecipes[btnIndex];
+        // 将全局变量中的菜谱修改为‘添加’
+        const recipes = this.data.recipes.map((item) => {
+            if (item.id === deleteRecipe.id) {
+                item.buttonText = '添加'
+            }
+            return item
+        })
+        this.setData({
+            recipes
+        })
+        this.onShow()
+        
     }
 })
